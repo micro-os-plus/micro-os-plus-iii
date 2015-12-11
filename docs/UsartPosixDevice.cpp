d1d9f5dadcabd5e44b3cd36a8f722b82094870c1
Liviu Ionescu <ilg@livius.net>
2015-12-11 14:51:19 +0200

    add C-HOWTO.md and samples

---
+++ docs/UsartPosixDevice.cpp
@@ -0,0 +1,60 @@
+#include "UsartPosixDevice.h"
+
+extern "C"
+{
+  int
+  usart_open (const char *path, int oflag, va_list args);
+
+  int
+  usart_close (void);
+
+  ssize_t
+  usart_read (void *buf, size_t nbyte);
+
+  ssize_t
+  usart_write (const void *buf, size_t nbyte);
+
+  int
+  usart_ioctl (int request, va_list args);
+}
+
+UsartPosixDevice::UsartPosixDevice (const char* name) :
+    os::PosixDevice (name)
+{
+  ;
+}
+
+UsartPosixDevice::~UsartPosixDevice ()
+{
+  ;
+}
+
+int
+UsartPosixDevice::doOpen (const char *path, int oflag, va_list args)
+{
+  return usart_open (path, oflags, args);
+}
+
+int
+UsartPosixDevice::doClose (void)
+{
+  return usart_close ();
+}
+
+ssize_t
+UsartPosixDevice::doRead (void *buf, size_t nbyte)
+{
+  return usart_read (buf, nbyte);
+}
+
+ssize_t
+UsartPosixDevice::doWrite (const void *buf, size_t nbyte)
+{
+  return usart_write (buf, nvyte);
+}
+
+int
+UsartPosixDevice::doIoctl (int request, va_list args)
+{
+  return usart_ioctl (request, args);
+}
---
+++ docs/UsartPosixDevice.h
@@ -0,0 +1,28 @@
+#include "posix-io/PosixDevice.h"
+
+class UsartPosixDevice : public os::PosixDevice
+{
+public:
+  UsartPosixDevice (const char* name);
+
+  ~UsartPosixDevice ();
+
+  virtual int
+  doOpen (const char *path, int oflag, va_list args);
+
+  virtual int
+  doClose (void);
+
+  virtual ssize_t
+  doRead (void *buf, size_t nbyte);
+
+  virtual ssize_t
+  doWrite (const void *buf, size_t nbyte);
+
+  virtual int
+  doIoctl (int request, va_list args);
+
+private:
+
+  // Add member variables, if needed
+};
