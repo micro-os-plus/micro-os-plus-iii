3cc28c097138dc23bf88f9e2fa2205837a3f5a6b
Liviu Ionescu <ilg@livius.net>
2015-12-16 18:44:08 +0200

    BlockDevice skeleton added

---
+++ include/posix-io/BlockDevice.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2015 Liviu Ionescu.
+ *
+ * µOS++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * µOS++ is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BLOCKDEVICE_H_
+#define BLOCKDEVICE_H_
+
+namespace os
+{
+  class BlockDevice
+  {
+  public:
+    BlockDevice () = default;
+
+  };
+} /* namespace os */
+
+#endif /* BLOCKDEVICE_H_ */
