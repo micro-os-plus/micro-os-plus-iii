1c6d657b62f861ed0c733eefb207e1ba2a0afb3f
Liviu Ionescu <ilg@livius.net>
2016-01-16 12:37:43 +0200

    add usbd-wrapper

---
+++ include/cmsis-plus/drivers/usbd-wrapper.h
@@ -0,0 +1,165 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2016 Liviu Ionescu.
+ *
+ * µOS++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * µOS++ is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CMSIS_DRIVER_USBD_WRAPPER_H_
+#define CMSIS_DRIVER_USBD_WRAPPER_H_
+
+#include <cmsis-plus/drivers/usb-device.h>
+
+// ----------------------------------------------------------------------------
+
+extern "C"
+{
+  // Avoid to include <Driver_USBD.h>
+  typedef void
+  (*ARM_USBD_SignalDeviceEvent_t) (uint32_t event); ///< Pointer to \ref ARM_USBD_SignalDeviceEvent : Signal USB Device Event.
+  typedef void
+  (*ARM_USBD_SignalEndpointEvent_t) (uint8_t ep_addr, uint32_t event); ///< Pointer to \ref ARM_USBD_SignalEndpointEvent : Signal USB Endpoint Event.
+
+  typedef struct _ARM_DRIVER_USBD const ARM_DRIVER_USBD;
+}
+
+namespace os
+{
+  namespace cmsis
+  {
+    namespace driver
+    {
+      // ======================================================================
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpadded"
+
+      // This wrapper makes a CMSIS USBD Keil driver behave like a
+      // CMSIS++ Serial driver.
+
+      class Usbd_wrapper : public usb::Device
+      {
+      public:
+
+        // --------------------------------------------------------------------
+
+        Usbd_wrapper (ARM_DRIVER_USBD* driver,
+                      ARM_USBD_SignalDeviceEvent_t c_cb_device_func,
+                      ARM_USBD_SignalEndpointEvent_t c_cb_endpoint_func)
+                          noexcept;
+
+        Usbd_wrapper (const Usbd_wrapper&) = delete;
+
+        Usbd_wrapper (Usbd_wrapper&&) = delete;
+
+        Usbd_wrapper&
+        operator= (const Usbd_wrapper&) = delete;
+
+        Usbd_wrapper&
+        operator= (Usbd_wrapper&&) = delete;
+
+        virtual
+        ~Usbd_wrapper () noexcept;
+
+        // --------------------------------------------------------------------
+
+      protected:
+
+        virtual const Version&
+        do_get_version (void) noexcept override;
+
+        const usb::device::Capabilities&
+        do_get_capabilities (void) noexcept override;
+
+        virtual status_t
+        do_power (Power state) noexcept override;
+
+        virtual status_t
+        do_connect (void) noexcept override;
+
+        virtual status_t
+        do_disconnect (void) noexcept override;
+
+        virtual usb::device::Status&
+        do_get_status (void) noexcept override;
+
+        virtual status_t
+        do_wakeup_remote (void) noexcept override;
+
+        virtual status_t
+        do_configure_address (usb::device::device_address_t dev_addr)
+            noexcept override;
+
+        virtual status_t
+        do_read_setup_packet (uint8_t* buf) noexcept override;
+
+        virtual usb::device::frame_number_t
+        do_get_frame_number (void) noexcept override;
+
+        virtual status_t
+        do_configure_endpoint (usb::endpoint_t ep_addr,
+                               usb::endpoint_type_t ep_type,
+                               usb::packet_size_t ep_max_packet_size)
+                                   noexcept override;
+
+        virtual status_t
+        do_unconfigure_endpoint (usb::endpoint_t ep_addr) noexcept override;
+
+        virtual status_t
+        do_stall_endpoint (usb::endpoint_t ep_addr, bool stall)
+            noexcept override;
+
+        virtual status_t
+        do_transfer (usb::endpoint_t ep_addr, uint8_t* data, std::size_t num)
+            noexcept override;
+
+        virtual std::size_t
+        do_get_transfer_count (usb::endpoint_t ep_addr) noexcept override;
+
+        virtual status_t
+        do_abort_transfer (usb::endpoint_t ep_addr) noexcept override;
+
+        // --------------------------------------------------------------------
+
+      private:
+
+        /// Pointer to CMSIS USBD Keil driver.
+        ARM_DRIVER_USBD* driver_;
+
+        /// Pointer to non-reentrant callback. Must be stored because
+        /// Initialise() is now delayed just before PowerControl(FULL).
+        ARM_USBD_SignalDeviceEvent_t c_cb_device_func_;
+        ARM_USBD_SignalEndpointEvent_t c_cb_endpoint_func_;
+
+        // Attempts to somehow use && failed, since the Keil driver
+        // functions return temporary objects. So the only portable
+        // solution was to copy these objects here and return
+        // references to these objects.
+        // (Not particularly proud of this solution, but could not find
+        // a better one.)
+
+        Version version_
+          { 0, 0 };
+        usb::device::Capabilities capa_;
+        usb::device::Status status_;
+
+      };
+
+#pragma GCC diagnostic pop
+
+    } /* namespace driver */
+  } /* namespace cmsis */
+} /* namespace os */
+
+#endif /* CMSIS_DRIVER_USBD_WRAPPER_H_ */
---
+++ src/cmsis-plus/drivers/usbd-wrapper.cpp
@@ -0,0 +1,190 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2016 Liviu Ionescu.
+ *
+ * µOS++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * µOS++ is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cmsis-plus/drivers/usbd-wrapper.h>
+
+#include <Driver_USBD.h>
+
+#include <cassert>
+#include <utility>
+#include <cstdio>
+
+// ----------------------------------------------------------------------------
+
+namespace os
+{
+  namespace cmsis
+  {
+    namespace driver
+    {
+      // ----------------------------------------------------------------------
+
+      Usbd_wrapper::Usbd_wrapper (
+          ARM_DRIVER_USBD* driver,
+          ARM_USBD_SignalDeviceEvent_t c_cb_device_func,
+          ARM_USBD_SignalEndpointEvent_t c_cb_endpoint_func) noexcept :
+      driver_ (driver),
+      c_cb_device_func_ (c_cb_device_func),
+      c_cb_endpoint_func_ (c_cb_endpoint_func)
+        {
+          ;
+        }
+
+      Usbd_wrapper::~Usbd_wrapper () noexcept
+      {
+        driver_ = nullptr;
+      }
+
+      // ----------------------------------------------------------------------
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waggregate-return"
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+
+      const Version&
+      Usbd_wrapper::do_get_version (void) noexcept
+      {
+        // Overwrite the C++ instance. Assume same layout.
+        *((ARM_DRIVER_VERSION*) (&version_)) = driver_->GetVersion ();
+        return version_;
+      }
+
+      const usb::device::Capabilities&
+      Usbd_wrapper::do_get_capabilities (void) noexcept
+      {
+        // Overwrite the C++ instance. Assume same layout.
+        *((ARM_USBD_CAPABILITIES*) (&capa_)) = driver_->GetCapabilities ();
+        return capa_;
+      }
+
+      usb::device::Status&
+      Usbd_wrapper::do_get_status (void) noexcept
+      {
+        // Overwrite the C++ instance. Assume same layout.
+        *((ARM_USBD_STATE*) (&status_)) = driver_->DeviceGetState ();
+        return status_;
+      }
+
+#pragma GCC diagnostic pop
+
+      status_t
+      Usbd_wrapper::do_power (Power state) noexcept
+      {
+        assert(driver_ != nullptr);
+
+        status_t status;
+
+        if (state == Power::full)
+          {
+            status = driver_->Initialize (c_cb_device_func_,
+                                          c_cb_endpoint_func_);
+            if (status != ARM_DRIVER_OK)
+              {
+                return status;
+              }
+          }
+
+        status = driver_->PowerControl ((ARM_POWER_STATE) state);
+
+        if (state == Power::off)
+          {
+            driver_->Uninitialize ();
+          }
+
+        return status;
+      }
+
+      status_t
+      Usbd_wrapper::do_connect (void) noexcept
+      {
+        return driver_->DeviceConnect ();
+      }
+
+      status_t
+      Usbd_wrapper::do_disconnect (void) noexcept
+      {
+        return driver_->DeviceDisconnect ();
+      }
+
+      status_t
+      Usbd_wrapper::do_wakeup_remote (void) noexcept
+      {
+        return driver_->DeviceRemoteWakeup ();
+      }
+
+      status_t
+      Usbd_wrapper::do_configure_address (
+          usb::device::device_address_t dev_addr) noexcept
+      {
+        return driver_->DeviceSetAddress (dev_addr);
+      }
+
+      status_t
+      Usbd_wrapper::do_read_setup_packet (uint8_t* buf) noexcept
+      {
+        return driver_->ReadSetupPacket (buf);
+      }
+
+      usb::device::frame_number_t
+      Usbd_wrapper::do_get_frame_number (void) noexcept
+      {
+        return driver_->GetFrameNumber ();
+      }
+
+      status_t
+      Usbd_wrapper::do_configure_endpoint (
+          usb::endpoint_t ep_addr, usb::endpoint_type_t ep_type,
+          usb::packet_size_t ep_max_packet_size) noexcept
+      {
+        return driver_->EndpointConfigure (ep_addr, ep_type, ep_max_packet_size);
+      }
+
+      status_t
+      Usbd_wrapper::do_unconfigure_endpoint (usb::endpoint_t ep_addr) noexcept
+      {
+        return driver_->EndpointUnconfigure (ep_addr);
+      }
+
+      status_t
+      Usbd_wrapper::do_stall_endpoint (usb::endpoint_t ep_addr, bool stall) noexcept
+      {
+        return driver_->EndpointStall (ep_addr, stall);
+      }
+
+      status_t
+      Usbd_wrapper::do_transfer (usb::endpoint_t ep_addr, uint8_t* data,
+                                 std::size_t num) noexcept
+      {
+        return driver_->EndpointTransfer (ep_addr, data, num);
+      }
+
+      std::size_t
+      Usbd_wrapper::do_get_transfer_count (usb::endpoint_t ep_addr) noexcept
+      {
+        return driver_->EndpointTransferGetResult (ep_addr);
+      }
+
+      status_t
+      Usbd_wrapper::do_abort_transfer (usb::endpoint_t ep_addr) noexcept
+      {
+        return driver_->EndpointTransferAbort (ep_addr);
+      }
+
+    } /* namespace driver */
+  } /* namespace cmsis */
+} /* namespace os */
