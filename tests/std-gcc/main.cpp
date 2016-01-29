56b7b664cb9a0815d5565b1c3c99200d4ab6dff1
Liviu Ionescu <ilg@livius.net>
2016-01-29 15:49:22 +0200

    update std test for chrono

--- tests/std-gcc/main.cpp
+++ tests/std-gcc/main.cpp
@@ -21,6 +21,7 @@
 
 #include <cstdio>
 #include <iostream>
+#include <chrono>
 
 using namespace os::cmsis;
 
@@ -44,7 +45,6 @@ void
 task1 (void)
 {
   trace::printf ("task1()\n");
-  ;
 }
 
 void
@@ -72,12 +72,17 @@ sleep_for_ticks (uint32_t)
 
 // ----------------------------------------------------------------------------
 
+#if !defined(OS_INCLUDE_CMSIS_THREAD_VARIADICS)
+#error OS_INCLUDE_CMSIS_THREAD_VARIADICS
+#endif
+
 int
 main (int argc, char* argv[])
 {
   using namespace ::std::chrono;
   using namespace os::cmsis::std;
   using namespace os::cmsis;
+  using namespace os;
 
   char c;
   thread th11
@@ -88,14 +93,18 @@ main (int argc, char* argv[])
   new int ();
 
   thread th12
-    { "th12", 777, task1 };
+    { new rtos::Thread
+      { "th12", 777, rtos::Priority::normal, task1 } };
 
   th12.native_handle ()->__run_function ();
 
   thread th13
-    { (char*) "th12", 777, task1 };
+    { new rtos::Thread
+      { (char*) "th13", 777, rtos::Priority::normal, task1 } };
 
   th13.native_handle ()->__run_function ();
+
+#if 1
 
   thread th21
     { task2, &c };
@@ -107,8 +116,14 @@ main (int argc, char* argv[])
 
   th31.native_handle ()->__run_function ();
 
-  this_thread::sleep_for (milliseconds
-    { 20 });
+  this_thread::sleep_for (microseconds (3001001));
+  this_thread::sleep_for (milliseconds (3001));
+  this_thread::sleep_for (seconds (3));
+
+#if 0
+  int n = 70;
+  this_thread::sleep_for (milliseconds (n));
+#endif
 
   th11.native_handle ()->set_priority (rtos::Priority::high);
 
@@ -117,6 +132,7 @@ main (int argc, char* argv[])
   th13.join ();
   th21.join ();
   th31.join ();
+#endif
 
   trace::printf ("%s done.\n", argv[0]);
   return 0;
