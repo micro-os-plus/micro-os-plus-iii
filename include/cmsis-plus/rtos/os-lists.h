d50cf0b0ec05712e7f61ac11d1e2f91e29fffdea
Liviu Ionescu <ilg@livius.net>
2016-05-13 22:58:35 +0300

    rtos/os-lists.h: fix doxygen param

--- include/cmsis-plus/rtos/os-lists.h
+++ include/cmsis-plus/rtos/os-lists.h
@@ -784,7 +784,7 @@ namespace os
 
       /**
        * @brief Add a new thread node to the list.
-       * @param [in] node Reference to a list node.
+       * @param [in] thread Reference to a list node.
        * @return Nothing.
        */
       void
