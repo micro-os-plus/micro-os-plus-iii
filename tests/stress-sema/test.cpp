6c9815e67ada6fa0c47f26eb82622fafeb8c983d
Liviu Ionescu <ilg@livius.net>
2016-05-17 11:42:51 +0300

    sema-stress: make loop forever

--- tests/stress-sema/test.cpp
+++ tests/stress-sema/test.cpp
@@ -16,7 +16,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include <cstring>
 #include <cmsis_device.h>
 
@@ -50,28 +49,30 @@ run_tests ()
       sema (period);
     }
 
-  trace::puts("\n\nRandom");
+  puts ("\n\nRandom");
 
   uint32_t from = 10;
   uint32_t to = tmr.in_clk_hz () / rtos::Systick_clock::frequency_hz / 32;
 
   for (i = 0; i < 100; ++i)
     {
-      uint64_t r = (uint64_t)rand ();
+      uint64_t r = (uint64_t) rand ();
       uint64_t period = r * (to - from) / RAND_MAX + from;
 
-      sema ((uint32_t)period);
+      sema ((uint32_t) period);
     }
 #endif
 
-  trace::puts("\nDone.");
+  puts ("Done.");
   return 0;
 }
 
 constexpr std::size_t max_count = 1000;
 //constexpr std::size_t max_count = 200;
-uint16_t volatile buf[max_count + 10];
-uint16_t volatile cnt;
+uint32_t volatile buf[max_count + 10];
+uint32_t volatile cnt;
+uint32_t volatile delayed;
+uint32_t volatile max_delayed;
 
 Semaphore sem;
 
@@ -80,6 +81,9 @@ sema_cb (void)
 {
   buf[cnt] = cnt;
   ++cnt;
+  delayed = 0;
+  max_delayed = 0;
+
 #if 1
   sem.post ();
 #endif
@@ -95,15 +99,16 @@ void
 sema (uint32_t cycles)
 {
   // Clear buffer
-  memset ((void*)buf, 0, sizeof(buf));
+  memset ((void*) buf, 0, sizeof(buf));
 
   cnt = 0;
+  delayed = 0;
 
   sem.reset ();
 
   tim_callback = sema_cb;
 
-  trace::printf ("\n%07d cy %07d Hz \n", cycles, tmr.in_clk_hz ()/cycles);
+  printf ("%7lu cy %4lu kHz ", cycles, tmr.in_clk_hz () / cycles / 1000);
 
   tmr.start (cycles);
 
@@ -111,12 +116,29 @@ sema (uint32_t cycles)
   uint32_t i;
   for (i = 0; i < max_count; ++i)
     {
-      sem.timed_wait (rtos::Systick_clock::frequency_hz);
+      assert(sem.timed_wait (rtos::Systick_clock::frequency_hz) == result::ok);
       trace_putchar ('-');
       assert(buf[i] == i);
+      delayed++;
+      if (delayed > max_delayed)
+        {
+          max_delayed = delayed;
+        }
     }
+
 #else
   while (cnt < max_count)
-    ;
+  ;
 #endif
+
+  // systick_clock.sleep_for (10);
+  max_delayed--;
+  if (max_delayed > 0)
+    {
+      printf ("%4lu late \n", max_delayed);
+    }
+  else
+    {
+      puts ("");
+    }
 }
