35d8b728e809112401ec548ab64bdc4938d33ae4
Liviu Ionescu <ilg@livius.net>
2016-05-17 20:16:11 +0300

    tests: add mutex-stress test

---
+++ tests/mutex-stress/main.cpp
@@ -0,0 +1,112 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2016 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <cmsis-plus/rtos/os.h>
+#include <cmsis-plus/diag/trace.h>
+
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <sys/time.h>
+
+#include <test.h>
+
+using namespace os;
+using namespace os::rtos;
+
+#if defined(__ARM_EABI__)
+
+void
+busy_wait (unsigned int micros)
+{
+  Systick_clock::current_t t;
+  systick_clock.now (&t);
+  uint64_t until_cycles = t.ticks * t.divisor + t.cycles
+      + t.core_frequency_hz * micros / 1000000;
+
+  uint64_t now_cycles;
+  do
+    {
+      systick_clock.now (&t);
+      now_cycles = static_cast<uint64_t> (t.ticks * t.divisor + t.cycles);
+    }
+  while (now_cycles < until_cycles);
+}
+
+#else
+
+void
+busy_wait (unsigned int micros)
+  {
+    struct timeval tp;
+    gettimeofday (&tp, nullptr);
+    uint64_t until_micros;
+    until_micros = static_cast<uint64_t> (tp.tv_sec * 1000000 + tp.tv_usec)
+    + micros;
+
+    uint64_t now_micros;
+    do
+      {
+        gettimeofday (&tp, nullptr);
+        now_micros = static_cast<uint64_t> (tp.tv_sec * 1000000 + tp.tv_usec);
+      }
+    while (now_micros < until_micros);
+  }
+
+#endif
+
+int
+os_main (int argc, char* argv[])
+{
+  trace::dump_args (argc, argv);
+
+  unsigned int seconds = 30;
+  if (argc > 1)
+    {
+      seconds = static_cast<unsigned int> (atoi (argv[1]));
+    }
+
+  printf ("Mutex stress & uniformity test.\n");
+
+  uint32_t seed;
+
+  int status;
+  struct timeval tp;
+  gettimeofday (&tp, nullptr);
+  // Use some large prime numbers and the current time.
+  // Must be a 32-bits value, to overflows and mess things further.
+  seed =
+      static_cast<uint32_t> ((tp.tv_sec + tp.tv_usec + 15485863) * 179424673);
+
+  printf ("Seed %u\n", static_cast<unsigned int> (seed));
+
+  srand (seed);
+
+  status = run_tests (seconds);
+  return status;
+}
+
