210cd3945a360962ba94d52d701e5fcbc50e9319
Liviu Ionescu <ilg@livius.net>
2018-04-06 11:29:23 +0300

    posix/termios.h: update for POSIX

--- include/cmsis-plus/posix/termios.h
+++ include/cmsis-plus/posix/termios.h
@@ -60,15 +60,13 @@ extern "C"
 {
 #endif
 
+// http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/termios.h.html
+
 // ----------------------------------------------------------------------------
 
-  /*
-   * Special Control Characters
-   *
-   * Index into c_cc[] character array.
-   *
-   *	Name	     Subscript	Enabled by
-   */
+// Special Control Characters
+
+// The following subscript names are for the array c_cc.
 #define	VEOF		0	/* ICANON */
 #define	VEOL		1	/* ICANON */
 #if __BSD_VISIBLE
@@ -83,7 +81,7 @@ extern "C"
 #define	VREPRINT 	6	/* ICANON together with IEXTEN */
 #define	VERASE2 	7	/* ICANON */
 #endif
-  /*			7	   ex-spare 1 */
+// 7	   ex-spare 1
 #define	VINTR		8	/* ISIG */
 #define	VQUIT		9	/* ISIG */
 #define	VSUSP		10	/* ISIG */
@@ -100,18 +98,20 @@ extern "C"
 #define	VTIME		17	/* !ICANON */
 #if __BSD_VISIBLE
 #define	VSTATUS		18	/* ICANON together with IEXTEN */
-  /*			19	   spare 2 */
+// 19	   spare 2
 #endif
-  /* Added 2017/08/18 (LNP) */
+
+// [LNP] Added 2017-08-18
 #define VTIME_MS        19      /* !ICANON */
-  /* End added */
+// [LNP] End added
+
 #define	NCCS		20
 
 #define	_POSIX_VDISABLE	0xff
 
-  /*
-   * Input flags - software input processing
-   */
+// Input Modes
+
+// The c_iflag field describes the basic terminal input control:
 #define	IGNBRK		0x00000001	/* ignore BREAK condition */
 #define	BRKINT		0x00000002	/* map BREAK to SIGINTR */
 #define	IGNPAR		0x00000004	/* ignore (discard) parity errors */
@@ -123,16 +123,15 @@ extern "C"
 #define	ICRNL		0x00000100	/* map CR to NL (ala CRMOD) */
 #define	IXON		0x00000200	/* enable output flow control */
 #define	IXOFF		0x00000400	/* enable input flow control */
-#if __BSD_VISIBLE
 #define	IXANY		0x00000800	/* any char will restart after stop */
+#if __BSD_VISIBLE
 #define	IMAXBEL		0x00002000	/* ring bell on input queue full */
 #endif
 
-  /*
-   * Output flags - software output processing
-   */
+// Output Modes
+
+// The c_oflag field specifies the system treatment of output:
 #define	OPOST		0x00000001	/* enable following output processing */
-#if __BSD_VISIBLE
 #define	ONLCR		0x00000002	/* map NL to CR-NL (ala CRMOD) */
 #define	TABDLY		0x00000004	/* tab delay mask */
 #define	    TAB0	    0x00000000	    /* no tab delay and expansion */
@@ -141,71 +140,22 @@ extern "C"
 #define	OCRNL		0x00000010	/* map CR to NL on output */
 #define	ONOCR		0x00000020	/* no CR output at column 0 */
 #define	ONLRET		0x00000040	/* NL performs CR function */
-#endif
 
-  /*
-   * Control flags - hardware control of terminal
-   */
-#if __BSD_VISIBLE
-#define	CIGNORE		0x00000001	/* ignore control flags */
-#endif
-#define	CSIZE		0x00000300	/* character size mask */
-#define	    CS5		    0x00000000	    /* 5 bits (pseudo) */
-#define	    CS6		    0x00000100	    /* 6 bits */
-#define	    CS7		    0x00000200	    /* 7 bits */
-#define	    CS8		    0x00000300	    /* 8 bits */
-#define	CSTOPB		0x00000400	/* send 2 stop bits */
-#define	CREAD		0x00000800	/* enable receiver */
-#define	PARENB		0x00001000	/* parity enable */
-#define	PARODD		0x00002000	/* odd parity, else even */
-#define	HUPCL		0x00004000	/* hang up on last close */
-#define	CLOCAL		0x00008000	/* ignore modem status lines */
-#if __BSD_VISIBLE
-#define	CCTS_OFLOW	0x00010000	/* CTS flow control of output */
-#define	CRTSCTS		(CCTS_OFLOW | CRTS_IFLOW)
-#define	CRTS_IFLOW	0x00020000	/* RTS flow control of input */
-#define	CDTR_IFLOW	0x00040000	/* DTR flow control of input */
-#define	CDSR_OFLOW	0x00080000	/* DSR flow control of output */
-#define	CCAR_OFLOW	0x00100000	/* DCD flow control of output */
-#endif
+// TODO: add
+// OFILL
+// NLDLY
+// CRDLY
+// TABDLY
+// BSDLY
+// VTDLY
+// FFDLY
 
-  /*
-   * "Local" flags - dumping ground for other state
-   *
-   * Warning: some flags in this structure begin with
-   * the letter "I" and look like they belong in the
-   * input flag.
-   */
+// Baud Rate Selection
 
-#if __BSD_VISIBLE
-#define	ECHOKE		0x00000001	/* visual erase for line kill */
-#endif
-#define	ECHOE		0x00000002	/* visually erase chars */
-#define	ECHOK		0x00000004	/* echo NL after line kill */
-#define	ECHO		0x00000008	/* enable echoing */
-#define	ECHONL		0x00000010	/* echo NL even if ECHO is off */
-#if __BSD_VISIBLE
-#define	ECHOPRT		0x00000020	/* visual erase mode for hardcopy */
-#define	ECHOCTL  	0x00000040	/* echo control chars as ^(Char) */
-#endif
-#define	ISIG		0x00000080	/* enable signals INTR, QUIT, [D]SUSP */
-#define	ICANON		0x00000100	/* canonicalize input lines */
-#if __BSD_VISIBLE
-#define	ALTWERASE	0x00000200	/* use alternate WERASE algorithm */
-#endif
-#define	IEXTEN		0x00000400	/* enable DISCARD and LNEXT */
-#define	EXTPROC         0x00000800      /* external processing */
-#define	TOSTOP		0x00400000	/* stop background jobs from output */
-#if __BSD_VISIBLE
-#define	FLUSHO		0x00800000	/* output being flushed (state) */
-#define	NOKERNINFO	0x02000000	/* no kernel output from VSTATUS */
-#define	PENDIN		0x20000000	/* XXX retype pending input (state) */
-#endif
-#define	NOFLSH		0x80000000	/* don't flush after interrupt */
-
-  /*
-   * Standard speeds
-   */
+// The input and output baud rates are stored in the termios structure.
+// These are the valid values for objects of type speed_t. The following
+// values shall be defined, but not all baud rates need be supported by
+// the underlying hardware.
 #define	B0	0
 #define	B50	50
 #define	B75	75
@@ -236,9 +186,69 @@ extern "C"
 #define	EXTB	38400
 #endif
 
-  /*
-   * Commands passed to tcsetattr() for setting the termios structure.
-   */
+// Control Modes
+
+// The c_cflag field describes the hardware control of the terminal;
+// not all values specified are required to be supported by the
+// underlying hardware.
+#if __BSD_VISIBLE
+#define CIGNORE   0x00000001  /* ignore control flags */
+#endif
+#define CSIZE   0x00000300  /* character size mask */
+#define     CS5       0x00000000      /* 5 bits (pseudo) */
+#define     CS6       0x00000100      /* 6 bits */
+#define     CS7       0x00000200      /* 7 bits */
+#define     CS8       0x00000300      /* 8 bits */
+#define CSTOPB    0x00000400  /* send 2 stop bits */
+#define CREAD   0x00000800  /* enable receiver */
+#define PARENB    0x00001000  /* parity enable */
+#define PARODD    0x00002000  /* odd parity, else even */
+#define HUPCL   0x00004000  /* hang up on last close */
+#define CLOCAL    0x00008000  /* ignore modem status lines */
+#if __BSD_VISIBLE
+#define CCTS_OFLOW  0x00010000  /* CTS flow control of output */
+#define CRTSCTS   (CCTS_OFLOW | CRTS_IFLOW)
+#define CRTS_IFLOW  0x00020000  /* RTS flow control of input */
+#define CDTR_IFLOW  0x00040000  /* DTR flow control of input */
+#define CDSR_OFLOW  0x00080000  /* DSR flow control of output */
+#define CCAR_OFLOW  0x00100000  /* DCD flow control of output */
+#endif
+
+// Local Modes
+
+// The c_lflag field of the argument structure is used to control
+// various terminal functions.
+// Warning: some flags in this structure begin with the letter "I"
+// and look like they belong in the input flag.
+#if __BSD_VISIBLE
+#define ECHOKE    0x00000001  /* visual erase for line kill */
+#endif
+#define ECHOE   0x00000002  /* visually erase chars */
+#define ECHOK   0x00000004  /* echo NL after line kill */
+#define ECHO    0x00000008  /* enable echoing */
+#define ECHONL    0x00000010  /* echo NL even if ECHO is off */
+#if __BSD_VISIBLE
+#define ECHOPRT   0x00000020  /* visual erase mode for hardcopy */
+#define ECHOCTL   0x00000040  /* echo control chars as ^(Char) */
+#endif
+#define ISIG    0x00000080  /* enable signals INTR, QUIT, [D]SUSP */
+#define ICANON    0x00000100  /* canonicalize input lines */
+#if __BSD_VISIBLE
+#define ALTWERASE 0x00000200  /* use alternate WERASE algorithm */
+#endif
+#define IEXTEN    0x00000400  /* enable DISCARD and LNEXT */
+#define EXTPROC         0x00000800      /* external processing */
+#define TOSTOP    0x00400000  /* stop background jobs from output */
+#if __BSD_VISIBLE
+#define FLUSHO    0x00800000  /* output being flushed (state) */
+#define NOKERNINFO  0x02000000  /* no kernel output from VSTATUS */
+#define PENDIN    0x20000000  /* XXX retype pending input (state) */
+#endif
+#define NOFLSH    0x80000000  /* don't flush after interrupt */
+
+// Attribute Selection
+
+// The following symbolic constants are for use with tcsetattr().
 #define TCSANOW         0               /* make change immediate */
 #define TCSADRAIN       1               /* drain output, then change */
 #define TCSAFLUSH       2               /* drain output, flush input */
@@ -246,9 +256,14 @@ extern "C"
 #define TCSASOFT        0x10            /* flag - don't alter h.w. state */
 #endif
 
+// Line Control
+
+// The following symbolic constants are for use with tcflush().
 #define TCIFLUSH        1
 #define TCOFLUSH        2
 #define TCIOFLUSH       3
+
+// The following symbolic constants are for use with tcflow().
 #define TCOOFF          1
 #define TCOON           2
 #define TCIOFF          3
@@ -256,17 +271,18 @@ extern "C"
 
 // ----------------------------------------------------------------------------
 
-  typedef unsigned int tcflag_t;
-  typedef unsigned char cc_t;
-  typedef unsigned int speed_t;
+  typedef unsigned int tcflag_t; // Terminal modes.
+  typedef unsigned char cc_t; // terminal special characters.
+  typedef unsigned int speed_t; // Terminal baud rates.
 
   struct termios
   {
-    tcflag_t c_iflag; /* input flags */
-    tcflag_t c_oflag; /* output flags */
-    tcflag_t c_cflag; /* control flags */
-    tcflag_t c_lflag; /* local flags */
+    tcflag_t c_iflag; /* input modes */
+    tcflag_t c_oflag; /* output modes */
+    tcflag_t c_cflag; /* control modes */
+    tcflag_t c_lflag; /* local modes */
     cc_t c_cc[NCCS]; /* control chars */
+
     speed_t c_ispeed; /* input speed */
     speed_t c_ospeed; /* output speed */
   };
